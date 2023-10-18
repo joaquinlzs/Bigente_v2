5fef775f508e3b3a53e2e1161393a8448e932b77
joaquinlzs <lazojoaquin.a@gmail.com>
2023-10-17 23:54:22 -0300

    import useDispatch in header component

--- app-bigente/src/components/Header.js
+++ app-bigente/src/components/Header.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import '../styles/Header.css';
+import { useDispatch } from 'react-redux';
 
 function Header() {
+    const dispatch = useDispatch();
+    
+    const handleButtonClick = (topic) => {
+        alert(`Has hecho click en el tópico ${topic}`);
+    };
+
     return (
         <header className="header">
             <h1 className="page-title">
@@ -11,10 +18,10 @@ function Header() {
 
             <nav className="navbar">
                 <ul className="nav-list">
-                    <li><button>Chile</button></li>
-                    <li><button>Energía</button></li>
-                    <li><button>Derecho</button></li>
-                    <li><button>Psicología</button></li>
+                    <li><button onClick={() => handleButtonClick("Chile")}>Chile</button></li>
+                    <li><button onClick={() => handleButtonClick("Energía")}>Energía</button></li>
+                    <li><button onClick={() => handleButtonClick("Derecho")}>Derecho</button></li>
+                    <li><button onClick={() => handleButtonClick("Psicología")}>Psicología</button></li>
                 </ul>
             </nav>
         </header>
